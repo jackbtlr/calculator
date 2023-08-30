c5a49a766732f4b6dbb49d42c5bc404313b618df
Jack B <16514207+jackbtlr@users.noreply.github.com>
2023-08-30 17:15:56 -0500

    Refactored code, added number shortening. Still not fully complete

--- app.js
+++ app.js
@@ -2,106 +2,164 @@ const screenBottom = document.querySelector("#screenBottom");
 const screenTop = document.querySelector("#screenTop");
 const clearButton = document.querySelector("#clear");
 const deleteButton = document.querySelector("#delete");
-const divide = document.querySelector("#divide");
-const multiply = document.querySelector("#multiply");
-const subtract = document.querySelector("#subtract");
-const add = document.querySelector("#add");
 const equals = document.querySelector("#equals");
+const decimal = document.querySelector("#decimal");
 
-let displayValue = 0;
-let firstNumber = 0;
-let secondNumber;
-let result;
-let operator;
+let currentValue = '0';
+let previousValue = '';
+let operator = '';
 
 const numButtons = document.querySelectorAll(".num");
 const operatorButtons = document.querySelectorAll(".oper");
 
-function operate(num1, num2, operator) {
+function operate(str1, str2, operator) {
+  let num1 = Number.parseFloat(str1);
+  let num2 = Number.parseFloat(str2);
   switch (operator) {
-    case "add":
-      return num1 + num2;
+    case "+":
+      return (num1 + num2).toString();
       break;
-    case "subtract":
-      return num1 - num2;
+    case "-":
+      return (num1 - num2).toString();
       break;
-    case "multiply":
-      return num1 * num2;
+    case "×":
+      return (num1 * num2).toString();
       break;
-    case "divide":
-      return num1 / num2;
-      break;
-  }
-}
-
-function operatorSymbol(operatorText) {
-  switch (operatorText) {
-    case "add":
-      return "+";
-      break;
-    case "subtract":
-      return "-";
-      break;
-    case "multiply":
-      return "×";
-      break;
-    case "divide":
-      return "÷";
+    case "÷":
+      return (num1 / num2).toString();
       break;
   }
 }
 
-for (let numButton of numButtons) {
-  numButton.addEventListener("click", () => {
-    const number = numButton.innerText;
-    if (`${displayValue}`.length < 10) {
-      if (displayValue === 0) {
-        displayValue = parseInt(number);
-      } else {
-        displayValue = parseInt(`${displayValue}${number}`);
-      }
-      screenBottom.innerText = displayValue;
-    }
+numButtons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    addDigit(e.target.innerText);
   });
-}
-
-for (let operatorButton of operatorButtons) {
-  operatorButton.addEventListener("click", (e) => {
-    if(screenTop.innerText){
-        let num1 = parseInt(screenTop.innerText.split(' ')[0]);
-        let num2 = displayValue 
-        displayValue = operate(num1, num2, operator);
-        screenBottom.innerText = displayValue;
-    }
-    operator = e.target.id;
-    firstNumber = displayValue;
-    screenTop.innerText = `${firstNumber} ${e.target.innerText}`;
-    displayValue = 0;
-  });
-}
-
-clearButton.addEventListener("click", () => {
-  displayValue = 0;
-  screenBottom.innerText = displayValue;
-  screenTop.innerText = "";
 });
 
-deleteButton.addEventListener("click", () => {
-  if (screenBottom.innerText.length === 1) {
-    displayValue = 0;
-    screenBottom.innerText = displayValue;
-  } else {
-    screenBottom.innerText = screenBottom.innerText.slice(0, -1);
-    displayValue = parseInt(screenBottom.innerText);
+operatorButtons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    addOperation(e.target.innerText);
+  })
+})
+
+equals.addEventListener('click', () => {
+  if(currentValue && previousValue && operator){
+    updateTopScreen(previousValue + " " + operator + " " + currentValue + " =");
+    currentValue = operate(previousValue, currentValue, operator)
+    currentValue = (Math.round(currentValue * 1000) / 1000).toString();
+    if (currentValue.length > 10){
+      screenBottom.innerText = scientific(currentValue, 10);
+    }
+    else{
+      screenBottom.innerText = currentValue;
+    }
+    operator = '';
+    previousValue = currentValue;;
   }
 });
 
-equals.addEventListener("click", () => {
-  secondNumber = displayValue;
-  result = operate(firstNumber, secondNumber, operator);
-  displayValue = result;
-  screenBottom.innerText = displayValue;
-  screenTop.innerText = `${firstNumber} ${operatorSymbol(
-    operator
-  )} ${secondNumber} =`;
+clearButton.addEventListener('click', () => {
+  currentValue = '';
+  previousValue = '';
+  operator = '';
+  screenBottom.innerText = '0';
+  screenTop.innerText = '';
 });
+
+function addDigit(digit) {
+  if (currentValue.length < 10) {
+    if (currentValue === '0' || currentValue === '') {
+      currentValue = digit;
+    } else {
+      currentValue += digit;
+    }
+    screenBottom.innerText = currentValue;
+  }
+}
+
+function addOperation(operatorText){
+  if(operator){
+    previousValue = operate(previousValue, currentValue, operator);
+  }
+  else if(currentValue){
+    previousValue = currentValue;
+  }
+  else{
+    previousValue = 0;
+  }
+  currentValue = '';
+  operator = operatorText;
+  screenTop.innerText = previousValue + " " + operatorText;
+}
+
+function scientific(numStr, space){
+  let power = (numStr.length - 1).toString();
+  return numStr[0] + "." + numStr.slice(1,space-1-power.length) + 'e' + power;
+  // return `${Math.round(Number.parseFloat(numStr) / (10 ** (power)))}e${power}`
+}
+
+decimal.addEventListener("click", () => {
+  updateTopScreen('1 + 2');
+});
+
+function updateTopScreen(str){
+  let [num1, operChar, num2] = str.split(' ')
+  if(str.length > 17){
+    num1 = scientific(num1, 7);
+    num2 = scientific(num2, 7);
+  }
+  screenTop.innerText = num1 + " " + operChar + " " + num2 + " =";
+}
+
+// function shortenNumber(numString){
+//   if(numString.split('.')[0].length > 10){
+
+//   }
+// }
+
+// function updateDisplay(topText, bottomText) {
+//   if (topText.length > 20) {
+//     screenTop.style.fontSize = "1.2em";
+//   }
+//   else{
+//     screenTop.style.fontSize = '1.8em'
+//   }
+//   if (bottomText.length > 10) {
+//     // bottomText = Math.round(Number.parseFloat(bottomText) * 1000) / 1000;
+//     if(bottomText.split('.')[0].length > 10){
+//       let overflow = bottomText.split('.')[0].length - 10;
+//       console.log(overflow);
+//       bottomText = `${Math.round(Number.parseFloat(bottomText) / (10 ** (overflow + 2)))} e${overflow + 2}`
+//     }
+//   }
+//   screenTop.innerText = topText;
+//   screenBottom.innerText = bottomText;
+// }
+
+// deleteButton.addEventListener('click', () => {
+//   if (screenBottom.innerText.length === 1) {
+//     displayValue = 0;
+//     screenBottom.innerText = displayValue;
+//   } else {
+//     screenBottom.innerText = screenBottom.innerText.slice(0, -1);
+//     displayValue = parseInt(screenBottom.innerText);
+//   }
+// });
+
+// for (let operatorButton of operatorButtons) {
+//   operatorButton.addEventListener("click", (e) => {
+//     if (screenTop.innerText) {
+//       let num1 = parseInt(screenTop.innerText.split(" ")[0]);
+//       let num2 = displayValue;
+//       displayValue = operate(num1, num2, operator);
+//       screenBottom.innerText = displayValue;
+//     }
+//     operator = e.target.id;
+//     firstNumber = displayValue;
+//     screenTop.innerText = `${firstNumber} ${e.target.innerText}`;
+//     displayValue = 0;
+//   });
+// }
+
+
